89aaa18d2a24b44ae0d099780d36f1224187aec8
Tim Keresey <tim.keresey@gmail.com>
2020-08-20 00:01:17 -0600

    complete Deck class

--- src/Deck.js
+++ src/Deck.js
@@ -3,8 +3,11 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Deck {
-  constructor(name) {
-
+  constructor(cards) {
+    this.cards = cards;
+  }
+  countCards() {
+    return this.cards.length;
   }
 };
 
